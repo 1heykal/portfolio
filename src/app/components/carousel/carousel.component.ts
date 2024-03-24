015e77b2aed3036533ccc5b151d79bbf2913abe4
Osama Heykal <oneheykal@gmail.com>
2024-03-24 23:47:56 +0200

    updated carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,72 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/Models/project';
-import { CarouselModule } from 'primeng/carousel'; 
-import { ButtonModule } from 'primeng/button'; 
-import { ImageModule } from 'primeng/image'; 
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
 import { FormsModule } from '@angular/forms';
-import { PrimeNGConfig } from 'primeng/api';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
-  selector: 'app-carousel',
-  standalone: true,
-  imports: [CarouselModule, ButtonModule, ImageModule, FormsModule],
-  templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+    selector: 'app-carousel',
+    standalone: true,
+    imports: [CarouselModule, ButtonModule, ImageModule, FormsModule, MatButtonModule, MatIconModule],
+    templateUrl: './carousel.component.html',
+    styleUrl: './carousel.component.scss'
 })
-export class CarouselComponent implements OnInit{
- 
-   projects: Project[] = [];
-  
-  constructor(){
-    this.projects = [
-      {
-        name: 'Home Compass Web API',
-        description: 'An app to help and empower the homeless.',
-        imgpath: 'assets/imgs/compass.png',
-        repoURL: 'https://github.com/1heykal/home-compass-api',
-        skills: [`Led the inception and execution of the Home Compass project, from requirements gathering to database
+export class CarouselComponent implements OnInit {
+
+    projects: Project[] = [];
+
+    constructor() {
+        this.projects = [
+            {
+                name: 'Home Compass Web API',
+                description: 'An app to help and empower the homeless.',
+                imgpath: 'assets/imgs/compass.png',
+                repoURL: 'https://github.com/1heykal/home-compass-api',
+                skills: [`Led the inception and execution of the Home Compass project, from requirements gathering to database
         schema design.`, ` Facilitated full CRUD operations and repository management, optimizing data access and manipulation
         efficiency.`, ` Implemented JWT Tokens and Refresh Tokens for enhanced privacy and security, ensuring compliance with
         data protection standards.`]
-      },
-      {
-        name: 'Home Compass Web API',
-        description: 'An app to help and empower the homeless.',
-        imgpath: 'assets/imgs/compass.png',
-        repoURL: 'https://github.com/1heykal/home-compass-api',
-        skills: [`Led the inception and execution of the Home Compass project, from requirements gathering to database
+            },
+            {
+                name: 'Home Compass Web API',
+                description: 'An app to help and empower the homeless.',
+                imgpath: 'assets/imgs/compass.png',
+                repoURL: 'https://github.com/1heykal/home-compass-api',
+                skills: [`Led the inception and execution of the Home Compass project, from requirements gathering to database
         schema design.`, ` Facilitated full CRUD operations and repository management, optimizing data access and manipulation
         efficiency.`, ` Implemented JWT Tokens and Refresh Tokens for enhanced privacy and security, ensuring compliance with
         data protection standards.`]
-      },
-      {
-        name: 'Home Compass Web API',
-        description: 'An app to help and empower the homeless.',
-        imgpath: 'assets/imgs/compass.png',
-        repoURL: 'https://github.com/1heykal/home-compass-api',
-        skills: [`Led the inception and execution of the Home Compass project, from requirements gathering to database
+            },
+            {
+                name: 'Home Compass Web API',
+                description: 'An app to help and empower the homeless.',
+                imgpath: 'assets/imgs/compass.png',
+                repoURL: 'https://github.com/1heykal/home-compass-api',
+                skills: [`Led the inception and execution of the Home Compass project, from requirements gathering to database
         schema design.`, ` Facilitated full CRUD operations and repository management, optimizing data access and manipulation
         efficiency.`, ` Implemented JWT Tokens and Refresh Tokens for enhanced privacy and security, ensuring compliance with
         data protection standards.`]
-      },
-      {
-        name: 'Home Compass Web API',
-        description: 'An app to help and empower the homeless.',
-        imgpath: 'assets/imgs/compass.png',
-        repoURL: 'https://github.com/1heykal/home-compass-api',
-        skills: [`Led the inception and execution of the Home Compass project, from requirements gathering to database
+            },
+            {
+                name: 'Home Compass Web API',
+                description: 'An app to help and empower the homeless.',
+                imgpath: 'assets/imgs/compass.png',
+                repoURL: 'https://github.com/1heykal/home-compass-api',
+                skills: [`Led the inception and execution of the Home Compass project, from requirements gathering to database
         schema design.`, ` Facilitated full CRUD operations and repository management, optimizing data access and manipulation
         efficiency.`, ` Implemented JWT Tokens and Refresh Tokens for enhanced privacy and security, ensuring compliance with
         data protection standards.`]
-      },
-    
-    ];
-}  
-    ngOnInit() { 
-        
-    } 
-} 
-export interface Tutorial { 
-    title?: String; 
-    image?: String; 
+            },
+
+        ];
+    }
+    ngOnInit() {
+
+    }
+}
+export interface Tutorial {
+    title?: String;
+    image?: String;
 }
