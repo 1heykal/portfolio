9560df0276ae1491ea7a229b19a4c88b001a49d3
Osama Heykal <oneheykal@gmail.com>
2024-03-25 23:28:26 +0200

    added training component

--- src/app/components/training/training.component.ts
+++ src/app/components/training/training.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCard, MatCardModule } from '@angular/material/card';
@@ -6,7 +7,7 @@ import { Training } from 'src/Models/training';
 @Component({
   selector: 'app-training',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule],
   templateUrl: './training.component.html',
   styleUrl: './training.component.scss'
 })
@@ -25,6 +26,19 @@ export class TrainingComponent {
         `Successfully completed assigned tasks and projects, contributing to the development of real-world
   applications.`
       ]
-    }];
+    },
+    {
+      title: ".NET Backend Development",
+      trainer: "ITI-Mansoura Summer Training",
+      start: "01-07-2023",
+      end: "",
+      details: [
+        `Engaged in hands-on learning experiences, applying C#, LINQ, Entity Framework, MSSQL, and ASP.NET Core
+  MVC.`,
+        `Successfully completed assigned tasks and projects, contributing to the development of real-world
+  applications.`
+      ]
+    }
+  ];
   }
 }
