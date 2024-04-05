77ee199e88f22c41685e0111111aff027be3f89c
Osama Heykal <oneheykal@gmail.com>
2024-04-05 21:49:44 +0200

    added education component

---
+++ src/app/components/education/education.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-education',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './education.component.html',
+  styleUrl: './education.component.scss'
+})
+export class EducationComponent {
+  
+}
--- src/app/components/engineer/engineer.component.ts
+++ src/app/components/engineer/engineer.component.ts
@@ -3,11 +3,12 @@ import { SkillsComponent } from '../skills/skills.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { TrainingComponent } from '../training/training.component';
+import { EducationComponent } from '../education/education.component';
 
 @Component({
   selector: 'app-engineer',
   standalone: true,
-  imports: [SkillsComponent, ProjectsComponent, CarouselComponent, TrainingComponent],
+  imports: [SkillsComponent, ProjectsComponent, CarouselComponent, TrainingComponent, EducationComponent],
   templateUrl: './engineer.component.html',
   styleUrl: './engineer.component.scss'
 })
